8f8c17f9bc4492372fd668de45b0e9adda98a9cd
stormGa <499536117@qq.com>
2023-03-28 00:43:21 +0800

    add code

--- 剑指offer/二进制中1的个数/Code.java
+++ 剑指offer/二进制中1的个数/Code.java
@@ -1,8 +1,18 @@
 package 剑指offer.二进制中1的个数;
 
 /**
+ * @desc 位运算
  * @author Kohath
  */
 public class Code {
-
+    public int hammingWeight(int n) {
+        int result = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                result = result + 1;
+            }
+            n = n >> 1;
+        }
+        return result;
+    }
 }
